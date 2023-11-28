0b3f3a14c1f2e71006274c5d51e9d454fad7b519
Michael <michaelj.silva@hotmail.com>
2023-11-28 22:26:47 +0000

    Added function to get a random element from an array

--- script.js
+++ script.js
@@ -118,7 +118,8 @@ function getPasswordOptions() {
 
 // Function for getting a random element from an array
 function getRandom(arr) {
-
+  var randomCharacter = Math.floor(Math.random() * arr.length);
+  return arr[randomCharacter];
 }
 
 // Function to generate password with user input
